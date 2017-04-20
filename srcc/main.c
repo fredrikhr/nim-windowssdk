38f05e3b535dd9d2dabaca78609290feb5f013d4
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-04-20 10:40:23 +0200

    Dummy C source for reference
    Including VSCode C/C++ header settings

---
+++ srcc/main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <Windows.h>
+#define SECURITY_WIN32
+#include <Security.h>
+
+int main(int argc, char* argv[])
+{
+	const int size = sizeof(SECURITY_STRING);
+	const auto v = SEC_DELETED_HANDLE;
+
+	void* ptr = NULL;
+
+	return EXIT_SUCCESS;
+}
+
