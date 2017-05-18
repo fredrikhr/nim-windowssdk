ac6b9d423d922371f5c4a0f545a5b7602e8837d7
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-05-18 23:56:08 +0200

    Generate documentation nimble task

--- src/windowssdk/shared/guiddef.nim
+++ src/windowssdk/shared/guiddef.nim
@@ -1,4 +1,4 @@
-##############################################################################
+##
 ##
 ## | Microsoft Windows
 ## | Copyright (c) Microsoft Corporation.  All rights reserved.
@@ -7,7 +7,7 @@
 ##
 ##  :Contents:   GUID definition
 ##
-##############################################################################
+##
 
 type Guid* = object
   ## ref.: https://msdn.microsoft.com/en-us/library/aa373931.aspx
@@ -16,10 +16,10 @@ type Guid* = object
   data4*: array[8, byte]
 
 template define_Guid*(name: untyped, long: uint32, w1, w2: uint16, b1, b2, b3, b4, b5, b6, b7, b8: byte): typed =
-  const name = Guid(data1: long, data2: w1, data3: w2, data4: [b1, b2, b3, b4, b5, b6, b7, b8])
+  const name* = Guid(data1: long, data2: w1, data3: w2, data4: [b1, b2, b3, b4, b5, b6, b7, b8])
 
 template define_OleGuid*(name: untyped, long: uint32, w1, w2: uint16): typed =
-  const name = Guid(data1: long, data2: w1, data3: w2, data4: [0xC0, 0, 0, 0, 0, 0, 0, 0x46])
+  const name* = Guid(data1: long, data2: w1, data3: w2, data4: [0xC0, 0, 0, 0, 0, 0, 0, 0x46])
 
 type Iid* = Guid
 type ClsId* = Guid
--- windowssdk.nimble
+++ windowssdk.nimble
@@ -10,6 +10,8 @@ srcDir        = "src"
 
 requires "nim >= 0.17.0"
 
+import strutils
+
 # Setup
 before build:
   mkDir "bin"
@@ -20,6 +22,23 @@ before test:
   mkDir "bin"
 before testWithSdk:
   mkDir "bin"
+
+task docall, "Document srcDir recursively":
+  proc recurseDir(srcDir, docDir: string, nimOpts: string = "") =
+    for srcFile in listFiles(srcDir):
+      if not srcFile.endsWith(".nim"):
+        echo "skipping non nim file: $#" % [srcFile]
+      let docFile = docDir & srcFile[srcDir.len..^5] & ".html"
+      echo "file: $# -> $#" % [srcFile, docFile]
+      exec "nim doc $# -o:\"$#\" \"$#\"" % [nimOpts, docFile, srcFile]
+    for srcSubDir in listDirs(srcDir):
+      let docSubDir = docDir & srcSubDir[srcDir.len..^1]
+      # echo "dir: $# -> $#" % [srcSubDir, docSubDir]
+      mkDir docSubDir
+      recurseDir(srcSubDir, docSubDir)
+
+  let docDir = "doc"
+  recurseDir(srcDir, docDir)
 
 task build, "Builds the Windows SDK bundle":
   exec "nim compile -o:bin/windowssdk --nimcache:obj src/windowssdk"
