f04e19e5b6d29bc46d198f0cf82a1f3c81350dde
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-08-14 01:13:00 +0200

    fixed header pragma

--- src/windowssdk/um/unknwn.nim
+++ src/windowssdk/um/unknwn.nim
@@ -4,12 +4,14 @@ import .. / importc_windowssdk
 
 when defined(useWindowsSdk):
   const sdkHeader = "<Unknwn.h>"
-  {.pragma: typeImportc, importc, header: sdkHeader.}
+  {.pragma: sdkHeader, header: "<Unknwn.h>".}
+  {.pragma: typeImportc, importc, sdkHeader.}
 else:
+  {.pragma: sdkHeader.}
   {.pragma: typeImportc.}
 
 whenUseWindowsSdk:
-  {.importc: "IID_IUnknown", header: sdkHeader.}
+  {.importc: "IID_IUnknown", sdkHeader.}
   const iid_IUnknown* : Iid = Iid(data1: 0x00000000, data2: 0x0000, data3: 0x0000, data4: [0xC0'u8, 0x00'u8, 0x00'u8, 0x00'u8, 0x00'u8, 0x00'u8, 0x00'u8, 0x46'u8])
 
 type
@@ -21,18 +23,18 @@ type
     lpVtbl {.importc.}: ptr IUnknownVtbl
 
 whenUseWindowsSdk:
-  {.importc: "IUnknown_QueryInterface", header: sdkHeader.}
+  {.importc: "IUnknown_QueryInterface", sdkHeader.}
   proc queryInterface*(this: ptr IUnknown, riid: ptr Iid, ppvObject: pointer): HResult = 
     this.lpVtbl.queryInterface(this, riid, ppvObject)
-  {.importc: "IUnknown_AddRef", header: sdkHeader.}
+  {.importc: "IUnknown_AddRef", sdkHeader.}
   proc addRef*(this: ptr IUnknown): HResult = 
     this.lpVtbl.addRef(this)
-  {.importc: "IUnknown_Release", header: sdkHeader.}
+  {.importc: "IUnknown_Release", sdkHeader.}
   proc release*(this: ptr IUnknown): HResult = 
     this.lpVtbl.release(this)
 
 whenUseWindowsSdk:
-  {.importc: "IID_AsyncIUnknown", header: sdkHeader.}
+  {.importc: "IID_AsyncIUnknown", sdkHeader.}
   const iid_AsyncIUnknown* : Iid = Iid(data1: 0x000e0000, data2: 0x0000, data3: 0x0000, data4: [0xC0'u8, 0x00'u8, 0x00'u8, 0x00'u8, 0x00'u8, 0x00'u8, 0x00'u8, 0x46'u8])
 
 type
@@ -47,24 +49,24 @@ type
   AsyncIUnknown* {.typeImportc.} = object
     lpVtbl {.importc.}: ptr AsyncIUnknownVtbl
 
-converter toIUnknown(x: ptr AsyncIUnknown): ptr IUnknown = cast[ptr IUnknown](x)
+converter toIUnknown*(x: ptr AsyncIUnknown): ptr IUnknown = cast[ptr IUnknown](x)
 
 whenUseWindowsSdk:
-  {.importc: "AsyncIUnknown_Begin_QueryInterface", header: sdkHeader.}
+  {.importc: "AsyncIUnknown_Begin_QueryInterface", sdkHeader.}
   proc begin_QueryInterface*(this: ptr AsyncIUnknown, riid: ptr Iid): HResult =
     this.lpVtbl.begin_QueryInterface(this, riid)
-  {.importc: "AsyncIUnknown_Finish_QueryInterface", header: sdkHeader.}
+  {.importc: "AsyncIUnknown_Finish_QueryInterface", sdkHeader.}
   proc finish_QueryInterface*(this: ptr AsyncIUnknown, ppvObject: pointer): HResult =
     this.lpVtbl.finish_QueryInterface(this, ppvObject)
-  {.importc: "AsyncIUnknown_Begin_AddRef", header: sdkHeader.}
+  {.importc: "AsyncIUnknown_Begin_AddRef", sdkHeader.}
   proc begin_AddRef*(this: ptr AsyncIUnknown): HResult =
     this.lpVtbl.begin_AddRef(this)
-  {.importc: "AsyncIUnknown_Finish_AddRef", header: sdkHeader.}
+  {.importc: "AsyncIUnknown_Finish_AddRef", sdkHeader.}
   proc finish_AddRef*(this: ptr AsyncIUnknown): HResult =
     this.lpVtbl.finish_AddRef(this)
-  {.importc: "AsyncIUnknown_Begin_Release", header: sdkHeader.}
+  {.importc: "AsyncIUnknown_Begin_Release", sdkHeader.}
   proc begin_Release*(this: ptr AsyncIUnknown): HResult =
     this.lpVtbl.begin_Release(this)
-  {.importc: "AsyncIUnknown_Finish_Release", header: sdkHeader.}
+  {.importc: "AsyncIUnknown_Finish_Release", sdkHeader.}
   proc finish_Release*(this: ptr AsyncIUnknown): HResult =
     this.lpVtbl.finish_Release(this)
